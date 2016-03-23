f2f04b5a94c38cc95d0bca1ff00e7e2b07446b83
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-03-23 18:33:18 +1100

    Update swag.js

--- swag.js
+++ swag.js
@@ -1,34 +1,70 @@
+var request = require('request');
+var Q = require('q');
+var bitcoin = require('bitcoinjs-lib');
+
+
 var PAYMENT_OUT_AMOUNT = 1;
-var TX_FEE = 8788; // current net fee at 22 Mar 2016
-var OP_RETURN_ATTACHED_DATA = '';
+var TX_FEE = 9000;
 
 var priv = '';
-var pub = '';
+var pub = '1DK9de3h3NVzWuLoHwiNeu2Ni9GyhWcPwD';
+var OP_RETURN_ATTACHED_DATA = 'jnk';
+
+
+var balanceInBits = 0;
 var inputTx = '';
 
-var balanceInBits;
+function bitcoinsToSatoshis(bitcoins) {
+	return bitcoins / 0.00000001;
+}
 
-function calcBal(bitcoins) {
-	balanceInBits = bitcoins / 0.00000001;
+function getBalanceAndTxsForAddress() {
+	var dfd = new Q.defer();
+
+	var options = {
+		url: "http://btc.blockr.io/api/v1/address/txs/" + pub
+	}
+	request(options, function(error, response, body) {
+		var data = JSON.parse(body);
+		dfd.resolve(data);
+	});
+
+	return dfd.promise;
 }
 
 
+getBalanceAndTxsForAddress()
+.then(function(balanceAndTxs) {
+	var txs = balanceAndTxs.data.txs;
+	txs.sort(function compare(a, b){
+		return b.amount - a.amount;
+	});
+
+	var chosenTx = txs[0];
+	inputTx = chosenTx.tx;
+	balanceInBits = bitcoinsToSatoshis(chosenTx.amount);
+
+	console.log("Loaded transaction "+inputTx+"\n with " + balanceInBits + " satoshis");
+})
+.then(function(){
+try{
+
+	var tx = new bitcoin.TransactionBuilder();
+	var data = new Buffer(OP_RETURN_ATTACHED_DATA);
+	var dataScript = bitcoin.script.nullDataOutput(data);
+
+	tx.addInput(inputTx, 1);
+	tx.addOutput(dataScript, PAYMENT_OUT_AMOUNT);
+	var changeAmount = balanceInBits - PAYMENT_OUT_AMOUNT - TX_FEE;
+	console.log("Outputs: "+changeAmount);
+	tx.addOutput(pub, changeAmount);
+
+	var keyPair = bitcoin.ECPair.fromWIF(priv)
+	tx.sign(0, keyPair);
 
 
+	console.log(tx.build().toHex());
 
-var bitcoin = require('bitcoinjs-lib');
-var tx = new bitcoin.TransactionBuilder();
+}catch(err){console.error(err)}
+})
 
-var data = new Buffer(opret);
-var dataScript = bitcoin.script.nullDataOutput(data);
-
-tx.addInput(inputTx, 1);
-tx.addOutput(dataScript, PAYMENT_OUT_AMOUNT);
-var changeAmount = balanceInBits - PAYMENT_OUT_AMOUNT - TX_FEE;
-tx.addOutput(pub, changeAmount);
-
-var keyPair = bitcoin.ECPair.fromWIF(priv)
-tx.sign(0, keyPair);
-
-
-console.log(tx.build().toHex());
